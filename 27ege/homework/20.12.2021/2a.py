b115c5e8df21728f7bde3d1a8594494b74df3ec7
master <dat.petr@gmail.com>
2021-12-21 20:12:32 +0300

    _

---
+++ 27ege/homework/20.12.2021/2a.py
@@ -0,0 +1,17 @@
+f = open('files/2A.txt', 'r')
+
+sum = 0
+mnd = 10001
+
+for i in range(7):
+    a, b = map(int, f.readline().split())
+    sum += max(a, b)
+    d = abs(a - b)
+
+    if d % 4 != 0:
+        mnd = min(d, mnd)
+
+if sum % 4 != 0:
+    print(sum)
+else:
+    print(sum - mnd)
