c3957582a5db015fd2bc85ecdc979408369d5107
master <dat.petr@gmail.com>
2022-02-03 13:27:49 +0300

    _

--- lessons/25ege/homework/1.02.2020/test.py
+++ lessons/25ege/homework/1.02.2020/test.py
@@ -1,3 +1,15 @@
-D = [2] + list(range(3, 5 + 1, 2))
+def alogoritm(n):
+    st_2 = bin(n)[2:]
 
-print(D)
+    for _ in range(2):
+        st_2 += str(st_2.count('1') % 2)
+
+    return st_2
+
+
+for i in range(1, 10000):
+    s = alogoritm(i)
+    if len(s) - len(bin(i)[2:]) == 2:
+        if int(int(s, 2)) > 63:
+            print(int(s, 2))
+            break
