fabe202aa28127edef9bb4b1cef3dfa7b9bd6e67
master <dat.petr@gmail.com>
2022-02-05 13:03:38 +0300

    _

--- lessons/8ege/homework/4.02.2022/4.py
+++ lessons/8ege/homework/4.02.2022/4.py
@@ -5,6 +5,7 @@ a_bad = set(list(permutations('ШРМБ', 3)))
 
 k = []
 st = ''
+count = 0
 
 for i in a_bad:
     for j in i:
@@ -14,21 +15,20 @@ for i in a_bad:
 
 print(k)
 
-count = 0
-
 for el in a:
     flag = True
     st2 = ''
-    for i in range(9):
+    for i in range(10):
         if el[i] in 'ШРМБ':
             st2 += el[i]
-            if st2 in k:
+            if st2 in k or len(st2) >= 3:
                 flag = False
                 break
         else:
             st2 = ''
 
     if flag:
+        print(el, end=' ')
         count += 1
 
 print(count)
---
+++ lessons/8ege/homework/4.02.2022/5.py
@@ -0,0 +1,39 @@
+from itertools import permutations, product
+
+a = set(list(permutations('ШУРУМБУРУМ', 10)))
+a_bad = set(list(permutations('ШРМБ', 3)))
+
+k = []
+st = ''
+count = 0
+
+for i in a_bad:
+    for j in i:
+        st += j
+    k.append(st)
+    st = ''
+
+print(k)
+
+for el in a:
+    flag = True
+    st2 = ''
+    for i in range(10):
+        if el[i] in 'ШРМБ':
+            st2 += el[i]
+            if st2 in k or len(st2) >= 3:
+                flag = False
+                break
+        else:
+            st2 = ''
+
+    if flag:
+        for i in range(9):
+            if el[i] == 'У' and el[i + 1] == 'У':
+                flag = False
+                break
+
+    if flag:
+        count += 1
+
+print(count)
