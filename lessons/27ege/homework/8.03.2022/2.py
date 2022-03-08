83d9768125e3fc94617830d859585ed5b550d4d2
master <dat.petr@gmail.com>
2022-03-08 16:04:07 +0300

    _

--- lessons/27ege/homework/8.03.2022/2.py
+++ lessons/27ege/homework/8.03.2022/2.py
@@ -1,10 +1,11 @@
-F = open('files/2a.txt', 'r')
-n = int(F.readline())
+f = open('files/2b.txt', 'r')
+n = int(f.readline())
 s = 0
 md1 = md2 = md3 = md4 = 10001
+ans = []
 
 for i in range(n):
-    a, b = map(int, (F.readline().split()))
+    a, b = map(int, (f.readline().split()))
     s = s + max(a, b)
     if (abs(a - b)) % 5 == 1 and abs(a - b) < md1:
         md1 = abs(a - b)
@@ -15,5 +16,18 @@ for i in range(n):
     if (abs(a - b)) % 5 == 4 and abs(a - b) < md4:
         md4 = abs(a - b)
 
+if s % 5 == 0:
+    print(s)
+else:
+    if (s - md1) % 5 == 0:
+        ans.append(s - md1)
+    if (s - md2) % 5 == 0:
+        ans.append(s - md2)
+    if (s - md3) % 5 == 0:
+        ans.append(s - md3)
+    if (s - md4) % 5 == 0:
+        ans.append(s - md4)
+    print(max(ans))
+
 print(s, md1, md2, md3, md4)
-F.close()
+f.close()
--- lessons/27ege/homework/8.03.2022/3.py
+++ lessons/27ege/homework/8.03.2022/3.py
@@ -1,21 +1,44 @@
-f = open('files/3b.txt', 'r')
-n = int(f.readline())
-m = []
+F = open('files/3a.txt', 'r')
+
+n = int(F.readline())
 s = 0
+md1 = md2 = md3 = md4 = md5 = md6 = md7 = 10001
+ans = []
 
-for i in range(n):
-    a = list(map(int, f.readline().split()))
-    a.sort()
-    m.append(a[1] - a[0])
-    m.append(a[2] - a[0])
-    s = s + a[1] + a[2]
+for _ in range(n):
+    a, b, c = map(int, (F.readline().split()))
+    s += max(a, b, c)
 
-if s % 8 == 0:
-    print(s)
-else:
-    m.sort()
-    j = 0
-    while (s - m[j]) % 8 != 0:
-        j += 1
-    print(s - m[j])
+    if (abs(a - b)) % 8 == 1 and abs(a - b) < md1:
+        md1 = abs(a - b)
+    if (abs(a - b)) % 8 == 2 and abs(a - b) < md2:
+        md2 = abs(a - b)
+    if (abs(a - b)) % 8 == 3 and abs(a - b) < md3:
+        md3 = abs(a - b)
+    if (abs(a - b)) % 8 == 4 and abs(a - b) < md4:
+        md4 = abs(a - b)
+    if (abs(a - b)) % 8 == 5 and abs(a - b) < md5:
+        md4 = abs(a - b)
+    if (abs(a - b)) % 8 == 6 and abs(a - b) < md6:
+        md4 = abs(a - b)
+    if (abs(a - b)) % 8 == 7 and abs(a - b) < md7:
+        md4 = abs(a - b)
 
+    if (abs(a - c)) % 8 == 1 and abs(a - c) < md1:
+        md1 = abs(a - c)
+    if (abs(a - c)) % 8 == 2 and abs(a - c) < md2:
+        md2 = abs(a - c)
+    if (abs(a - c)) % 8 == 3 and abs(a - c) < md3:
+        md3 = abs(a - c)
+    if (abs(a - c)) % 8 == 4 and abs(a - c) < md4:
+        md4 = abs(a - c)
+    if (abs(a - c)) % 8 == 5 and abs(a - c) < md5:
+        md4 = abs(a - c)
+    if (abs(a - c)) % 8 == 6 and abs(a - c) < md6:
+        md4 = abs(a - c)
+    if (abs(a - c)) % 8 == 7 and abs(a - c) < md7:
+        md4 = abs(a - c)
+
+print(s, md1, md2, md3, md4, md5, md6, md7)
+
+F.close()
