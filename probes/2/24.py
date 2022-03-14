b5bb03833348fc12c9152b25a04870b567859e00
master <dat.petr@gmail.com>
2022-03-14 13:09:37 +0300

    _

--- probes/2/24.py
+++ probes/2/24.py
@@ -14,14 +14,17 @@ for i in a:
     if i == 1:
         length += 1
         count += 1
+
         while count < 3:
             if i == 1:
                 count += 1
                 length += 1
             else:
                 length += 1
+
         if length > mx:
             mx = length
+
         length = 0
         count = 0
 
