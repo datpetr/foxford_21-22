e785a12d6e7d5c988bdd0ff53b88f48949ae0e95
master <dat.petr@gmail.com>
2022-03-14 13:07:01 +0300

    _

--- probes/2/24.py
+++ probes/2/24.py
@@ -10,11 +10,19 @@ for i in range(len(f)):
     if f[i] == 'F':
         a[i] = 1
 
-for i, val in enumerate(a):
-    if val == 1:
+for i in a:
+    if i == 1:
+        length += 1
         count += 1
         while count < 3:
-            if val == 1:
+            if i == 1:
                 count += 1
+                length += 1
             else:
+                length += 1
+        if length > mx:
+            mx = length
+        length = 0
+        count = 0
 
+print(mx + 1)
