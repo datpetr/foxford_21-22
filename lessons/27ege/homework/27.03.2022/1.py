5352acaadbc3a252407008d5baeb3c5aebd6587a
master <dat.petr@gmail.com>
2022-03-28 20:05:30 +0300

    _

---
+++ lessons/27ege/homework/27.03.2022/1.py
@@ -0,0 +1,33 @@
+def simple(n):
+    if 0 < n < 2:
+        return False
+    else:
+        for i in range(2, int(n ** 0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
+
+
+f = open('files/1a.txt', 'r')
+n = int(f.readline())
+
+s = 0
+ks = 0
+a = [0] * 6
+ms = 0
+
+for i in range(n):
+    x = int(f.readline())
+    s += x
+
+    if simple(x):
+        ks += 1
+    r = ks % 6
+
+    if a[r] != 0 and (s - a[r]) > ms:
+        ms = s - a[r]
+
+    if a[r] == 0:
+        a[r] = s
+
+print(ms)
