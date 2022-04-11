fc19a4f2ffe8d7578ded2760ab5356c68ce5260b
master <dat.petr@gmail.com>
2022-04-11 22:04:50 +0300

    _

--- probes/dosrok/12.py
+++ probes/dosrok/12.py
@@ -4,6 +4,6 @@ while '1111' in s or '8888' in s:
     if '1111' in s:
         s = s.replace('1111', '8', 1)
     else:
-        s = s.replace('88888', '11', 1)
+        s = s.replace('8888', '11', 1)
 
 print(s)
