fead278cc462e4fcc55a54790e48e4df819587e8
master <dat.petr@gmail.com>
2022-04-27 14:46:37 +0300

    _

---
+++ lessons/26ege/homework/27.04.2022/1.py
@@ -0,0 +1,25 @@
+f = open('files/1.txt', 'r')
+
+n, s = list(map(int, f.readline().split()))
+data = []
+s_ab = count = 0
+
+for i in range(n):
+    a, b, c = f.readline().split()
+    data.append([int(a), int(b), str(c)])
+
+f.close()
+
+data.sort(key=lambda x: (x[2], x[0]))
+
+xSum = 0
+countZ = 0
+for d in data:
+    if xSum + d[0] > s:
+        break
+    thisCount = min(d[1], int((s - xSum) / d[0]))
+    xSum += d[0] * thisCount
+    if d[2] == 'B':
+        countZ += thisCount
+
+print(countZ, s - xSum)
--- lessons/8ege/homework/4.02.2022/4_good.py
+++ lessons/8ege/homework/4.02.2022/4_good.py
@@ -9,7 +9,7 @@ for el in a:
     flag = True
     st2 = ''
     for i in range(8):
-        if el[i] in ' ШРМБ' and el[i+1] in 'ШРМБ' and el[i+2] in 'ШРМБ':
+        if el[i] in ' ШРМБ' and el[i + 1] in 'ШРМБ' and el[i + 2] in 'ШРМБ':
             flag = False
             break
 
