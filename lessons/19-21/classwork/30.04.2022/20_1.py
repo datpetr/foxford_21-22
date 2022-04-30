cfb1296663035be20193adc02fb90c7ffb8ca367
master <dat.petr@gmail.com>
2022-04-30 16:48:10 +0300

    _

---
+++ lessons/19-21/classwork/30.04.2022/20_1.py
@@ -0,0 +1,31 @@
+def f(x, pos, prev):  # prev +1 -- 1; +2 -- 2; x * 2 -- 3
+    if x >= 43 and pos == 3:
+        return True
+    elif x < 43 and pos == 3:
+        return False
+    elif x >= 43 and pos == 2:
+        return False
+
+    if pos % 2 == 0:
+        if prev == 1:
+            return f(x + 2, pos + 1, 2) or f(x * 2, pos + 1, 3)
+        if prev == 2:
+            return f(x + 1, pos + 1, 1) or f(x * 2, pos + 1, 3)
+        if prev == 3:
+            return f(x + 1, pos + 1, 1) or f(x + 2, pos + 1, 2)
+        else:
+            return f(x + 2, pos + 1, 2) or f(x * 2, pos + 1, 3) or f(x + 1, pos + 1, 1)
+    else:
+        if prev == 1:
+            return f(x + 2, pos + 1, 2) and f(x * 2, pos + 1, 3)
+        if prev == 2:
+            return f(x + 1, pos + 1, 1) and f(x * 2, pos + 1, 3)
+        if prev == 3:
+            return f(x + 1, pos + 1, 1) and f(x + 2, pos + 1, 2)
+        else:
+            return f(x + 2, pos + 1, 2) and f(x * 2, pos + 1, 3) and f(x + 1, pos + 1, 1)
+
+
+for s in range(1, 43):
+    if f(s, 0, 0):
+        print(s)
